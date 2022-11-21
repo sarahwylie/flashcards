e8cc1f642f3e51c7fdcf33641951f057d388ca49
sarahwylie <sarahannemariawylie@gmail.com>
2022-11-21 14:55:42 -0600

    added new questions

--- src/components/Queries.js
+++ src/components/Queries.js
@@ -26,7 +26,26 @@ const queries = [
     {
       id: 5,
       question: 'What is the different between let, const, and var?',
-      
+      answer: 'Var: global scope when declared outside a function, function scope when declared within, can be updated and re-declared, problematic because it may be unknowingly overwritten. Let: block scoped, can be updated but not re-declared. Const: block scoped, cannot be updated or re-declared.',
+      source: 'https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/'
+    },
+    {
+      id: 6,
+      question: 'What is Recursion in a computer science and coding context?',
+      answer: 'The process in which a function calls itself directly or indirectly. A recursive function solves a particular problem by calling a copy of itself and solving smaller subproblems of the original problems. Example: https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/',
+      source: 'https://www.geeksforgeeks.org/introduction-to-recursion-data-structure-and-algorithm-tutorials/'
+    },
+    {
+      id: 7,
+      question: 'What is Bubble Sorting and how does it work?',
+      answer: 'Bubble sorting involves comparing adject elements and swapping them if they are not in order. This algorithm is not suitable for large data sets.',
+      source: 'https://www.tutorialspoint.com/data_structures_algorithms/bubble_sort_algorithm.htm'
+    },
+    {
+      id: 8, 
+      question: 'What is a Linked List?',
+      answer: 'A linear data structure in which elements are linked using pointers. The Linked List consists of nodes, and each node contains a data field and a reference link to the next node in the list.',
+      source: 'https://www.geeksforgeeks.org/data-structures/linked-list/'
     }
   ]
 
