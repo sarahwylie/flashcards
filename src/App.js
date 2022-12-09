bd8c0de402086fc36fe24848ae112661dbf5a55e
sarahwylie <sarahannemariawylie@gmail.com>
2022-12-09 13:45:34 -0600

    not getting far here...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import Flashcards from './components/Flashcards';
 import queries from './components/Queries';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './index.css'
 
 const App = () => {
 
   return (
-    // <Container>
-    //   <Row>
-      <div className="card-container">
+    <Container>
+      <Row>
+      <Col className="card-container" sm={8}>
       {queries.map(query => {
             return <Flashcards key={query.id} query={query} />
           })}
-        </div>
-    //   </Row>
-    // </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
