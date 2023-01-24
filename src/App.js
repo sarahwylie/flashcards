088f2b753b4689b8d1f870c177b360481ddd9719
sarahwylie <sarahannemariawylie@gmail.com>
2023-01-24 09:37:09 -0600

    moving to a new computer

--- src/App.js
+++ src/App.js
@@ -12,26 +12,27 @@ import Paper from '@mui/material/Paper';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  // ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
 const App = () => {
-  // const [flip, setFlip] = useState(false);
+// const [categories] = useState([
+//   {name: "Computer Science"},
+//   {name: "React"},
+
+// ])
 
   return (
     <Box sx={{ flexGrow: 2 }}>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {/* <Grid xs={12} xl={3}> */}
-        {/* <Item className="card-container"> */}
+
         {queries.map(query => {
           return <Grid item xs={2} sm={4} md={4}>
             <Item><Flashcards query={query} key={query.id}/></Item></Grid>
         })}
-        {/* </Item> */}
-        {/* </Grid> */}
+
       </Grid>
     </Box>
   );
